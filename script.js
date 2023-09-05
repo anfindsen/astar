37741799927b31eeff5156186ba4a815904dde3d
Patrick Nikolay Falkeid <patrickfalkeid@gmail.com>
2023-09-05 21:55:29 +0200

    bug: introduced new bug pathfinding

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 "use strict";
-const fileReader = new FileReader()
+const fileReader = new FileReader();
 
 class Tile {
     constructor(x, y,cost){
-        this.cost = cost;
-        this.visited = false;
+        this.cost = Number.parseInt(cost);
         this.visitable = cost > -1;
         this.x = x;
         this.y = y;
+        this.open = false;
+        this.closed = false;
+        this.path = false;
+        this.isStartTile = false;
+        this.isDestTile = false;
+    }
+    
+    setOpen() {
+        if (!this.visitable) throw new Error("Node is not visitable");
+        this.open = true;
+        this.closed = false;
+    }
+    setClosed() {
+        if (!this.visitable) throw new Error("Node is not visitable");
+        this.open = false;
+        this.closed = true;
     }
 }
 
 const colors = {
-    visited:        'rgb(200, 200, 200)',
+    open:           'rgb(200, 200, 200)',
+    closed:         'rgb(150, 150, 150)',
     unexplored:     'rgb(255, 255, 255)',
     path:           'rgb(255, 255,   0)',
     unvisitable:    'rgb(255,   0,   0)',
-    text:           'rgb(  0,   0,   0)'
+    start:          'rgb(  0,   0, 255)',
+    dest:           'rgb(255, 192, 203)',
+    text:           'rgb(  0,   0,   0)',
 }
 
 class Map{
@@ -24,10 +42,6 @@ class Map{
         this.tiles2d = tiles2d;
         this.height = tiles2d.length;
         this.width = tiles2d[0].length;
-    }
-
-    visit(x,y) {
-        this.getTile(x,y).visited = true;
     }
     
     visitable(x,y) {
@@ -58,6 +72,17 @@ class MapCanvas{
         this.canvas.width = this.width * this.tileSize;
         this.parent.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
+        
+        const logTileUnderCursor = (e, tileSize) => {
+            const cursorX = e.offsetX;
+            const cursorY = e.offsetY;
+            console.log("X: " + Math.floor(cursorX/tileSize) + "\nY: " + 
+            Math.floor(cursorY/tileSize));
+        }
+        const onClick = (e) => logTileUnderCursor(e, tileSize);
+        this.canvas.onclick = this.logTileUnderCursor;
+        this.canvas.addEventListener('click', onClick);
+
     }
 
     drawMap() {
@@ -68,10 +93,19 @@ class MapCanvas{
     }
 
     drawTile(tile) {
-        const visitableColor = tile.visited ? colors.visited : colors.unexplored;
-        const color = tile.visitable ? visitableColor : colors.unvisitable;
+
+        const getColor = (tile) => {
+            if(tile.startTile) return colors.start;
+            if(tile.destTile) return colors.dest;
+            if(tile.path) return colors.path;
+            if(tile.closed) return colors.closed;
+            if(tile.open) return colors.open;
+            if(!tile.visitable) return colors.unvisitable;
+            return colors.unexplored;
+        }
+        const color = getColor(tile);
         const xCoord = this.tileSize * tile.x;
-        const yCoord = this.tileSize * tile.y;
+        const yCoord = this.tileSize * (tile.y);
         this.ctx.fillStyle = color;
         this.ctx.fillRect(xCoord, yCoord, this.tileSize, this.tileSize);
         this.ctx.strokeStyle = '1px solid black';
@@ -80,53 +114,98 @@ class MapCanvas{
         this.ctx.textBaseline = 'middle';
         this.ctx.textAlign = 'center';
         if (tile.visitable){
-            this.ctx.fillText(tile.cost, xCoord + this.tileSize / 2, yCoord + this.tileSize / 2)
+            this.ctx.fillText(tile.cost, xCoord + this.tileSize / 2, yCoord +
+                this.tileSize / 2)
             this.ctx.strokeRect(xCoord, yCoord, this.tileSize, this.tileSize);
         }
     }
 
-    visit(x,y){
-        if(this.map.visitable(x,y)){
-            this.map.visit(x,y);
-            this.drawTile(x,y);
-        }
-        else {
-            console.error("Nei fysj og fy nå prøvde du å besøga ein tile som ikkje går an å besøga kompis")
-        }
-    }
-
     aStar(startNode, destNode) {
+        startNode.startTile = true;
+        destNode.destTile = true;
+        // 1 Open list starts at start node
         const open = [{node: startNode,
-        g: 0}];
+        g: 0, 
+        parent: null}];
         const closed = [];
-        const successorNodes = [];
-        const currentNode = null;
-        const comparator = (node, prevNode) => node.g - prevNode.g;
-        while(open.length) {
-            open.sort(comparator);
-            currentNode = open.pop()
-            if (currentNode == destNode){
-                console.log("u win, congrat")
-                break
+        let currentNode = null;
+        const nodeInit = (node) => {
+            return {node, g: 0, parent: null}
+        }
+
+        const traverseParents = (node) => {
+            if (node) {
+                console.log(node);
+                node.node.path = true;
+                traverseParents(node.parent);
             }
-            successorNodes += [
-                this.map.getTile(currentNode.x,currentNode.y-1),
-                this.map.getTile(currentNode.x-1,currentNode.y),
-                this.map.getTile(currentNode.x,currentNode.y+1),
-                this.map.getTile(currentNode.x+1,currentNode.y),
-            ].filter(node);
+        };
+        
+        const comparator = (node, prevNode) => f(prevNode, destNode) - f(node, destNode);
+        
+        // 2 While open list is not empty
+        while(open.length) {
+            // 3,4 Take node from open with lowest f. Assumes open array is sorted descending w.r.t. f.
+            currentNode = open.pop();
+
+            // 5 If the current node is the destination node we have found a solution. 
+            if (currentNode.node === destNode){
+                console.log("u win, congrat");
+                traverseParents(currentNode);
+                this.drawMap()
+                break;
+            }
+            
+            // 6 Get all nodes one can visit from current node
+            const successorNodes = [
+                nodeInit(this.map.getTile(currentNode.node.x,currentNode.node.y-1)),
+                nodeInit(this.map.getTile(currentNode.node.x-1,currentNode.node.y)),
+                nodeInit(this.map.getTile(currentNode.node.x,currentNode.node.y+1)),
+                nodeInit(this.map.getTile(currentNode.node.x+1,currentNode.node.y)),
+            ].filter(currentNode => {
+                return currentNode.node.cost > -1
+            });
+            // 7 Check all possible next nodes
             successorNodes.forEach(successorNode => {
-                // If open contains successornode
+                // 8 Set cost of travelling to successorNode to current node + cost 
+                const successorCurrCost = currentNode.g + successorNode.cost
+                
+                // 9 If open contains successornode
                 if (open.filter(node => node.node === successorNode.node).length) {
-                    if(successorNode.g > currentNode.g + successorNode.cost) {
-                        successorNode.g = currentNode.g + successorNode.cost
+                    // 10
+                    if (successorCurrCost> currentNode.g + successorNode.cost) {
+                        return; // 10 Continue to next step in forEach iteration
                     }
                 }
-                else{
+                // 11 If closed contains successor node and this path is cheaper
+                else if (closed.filter(node => node.node === successorNode.node).length) {
+                    // 13
+                    if (successorCurrCost > currentNode.g + successorNode.cost){
+                        const i = closed.findIndex(node => node.node === successorNode.node);
+                        closed.splice(i, 1); // Remove element from closed
+                        open.push(successorNode);
+                        successorNode.node.setOpen();
+                        open.sort(comparator);
+                    }
+                    // 12
+                    else return;
+                }
+                // 14
+                else {
+                    // 15
                     open.push(successorNode)
+                    successorNode.node.setOpen()
                     open.sort(comparator);
                 }
+                successorNode.g = successorCurrCost; // 18
+                successorNode.parent = currentNode; // 19
             });
+            closed.push(currentNode) // 21
+            currentNode.node.setClosed()
+            this.drawMap();
+        }
+        if (currentNode.node !== destNode) {
+            console.error('Det finnes ingen vei.');
         }
     }
 }
@@ -148,21 +227,29 @@ function setcsv() {
     reader.addEventListener(
       "load",
       () => {
-        const blobStr = reader.result
-        const array = blobStr.split("\n").map((row) => row.split(","));
-        const tiles2d = array.map((row, y) => row.map((number, x) => new Tile(x, y, number))).filter(row => row.length > 1);
-        const map = new Map(tiles2d);
-        const mapCanvas = new MapCanvas(map, canvasDiv, 30);
-        mapCanvas.drawMap();
-        mapCanvas.visit(2, 9);
+        const blobStr = reader.result;
+        mazeInit(blobStr);
       },
       false,
     );
-  
+
     if (file) {
       reader.readAsText(file);
     }
 }
+
+function mazeInit(blobStr) {
+    const array = blobStr.split("\n").map((row) => row.split(",")).filter(row => row.length > 1);
+    const tiles2d = array.map((row, y) => row.map((number, x) => new Tile(x, y, number))).filter(row => row.length > 1);
+    const map = new Map(tiles2d);
+    const mapCanvas = new MapCanvas(map, canvasDiv, 30);
+    mapCanvas.drawMap();
+    mapCanvas.aStar(map.getTile(35, 41), map.getTile(32, 28));
+    mapCanvas.drawMap();
+}
+
+const blobStr = document.getElementById('data').innerHTML;
+mazeInit(blobStr);
 
 /** Heuristic function implementing Euclidian distance 
  * uses like pythagoras and such
@@ -170,5 +257,12 @@ function setcsv() {
 function h(startTile, destTile) {
     const deltaX = destTile.x - startTile.x;
     const deltaY = destTile.y - startTile.y;
-    return Math.sqrt(deltaX ** 2 + deltaY ** 2);
+    return Math.abs(deltaX) + Math.abs(deltaY);
+}
+
+/**
+ * Assumes input nodes on form {node, g, parent}
+ */
+function f(currentNode, destNode) {
+    return h(currentNode.node, destNode) + currentNode.g;
 }
